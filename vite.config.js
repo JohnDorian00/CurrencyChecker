17e1b7253024a89c15114687133f767004923432
JohnDorian00 <remailfunny@gmail.com>
2023-09-27 23:36:29 +0200

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CurrencyChecker/',
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
